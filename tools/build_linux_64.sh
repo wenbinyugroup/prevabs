e391cca4ecde47a1f456452a68a13416abf6e469
Su Tian <tian50@msg.ecn.purdue.edu>
2025-10-03 00:04:22 -0400

    update linux build script

--- tools/build_linux_64.sh
+++ tools/build_linux_64.sh
@@ -5,10 +5,10 @@ cd build_linux_64
 cmake3 \
   -DCMAKE_C_COMPILER=gcc \
   -DCMAKE_CXX_COMPILER=g++ \
-  -DCMAKE_PREFIX_PATH="/package/gcc/7.1.0" \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=.. \
-  ..
+  .. \
+  --debug-find
 
 make -j4
 make install
