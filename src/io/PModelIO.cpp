e359394af702038bfee3661a8bf4f65880954b8c
Su Tian <sutian@analyswift.com>
2026-07-06 15:45:37 -0400

    fix linux build issue

--- src/io/PModelIO.cpp
+++ src/io/PModelIO.cpp
@@ -37,9 +37,9 @@
 #elif _WIN32
 #include <tchar.h>
 #include <windows.h>
+#endif
 
 using namespace dcel;  // phase 0: DCEL types moved to namespace dcel
-#endif
 
 int readInputMain(const std::string &filenameCrossSection,
                   const std::string &filePath, PModel *pmodel) {
